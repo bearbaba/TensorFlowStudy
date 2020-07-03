6948fc0bb949696b9eb4348b14036a3b1792a403
bearbaba <2200522850@qq.com>
2020-07-03 19:06:53 +0800

    分类算法，segmoid函数

--- 4分类问题/demo.py
+++ 4分类问题/demo.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
-from tensorflow.keras import datasets
+from tensorflow.keras import datasets, layers, optimizers
+
 
 import numpy as np
-
 
 # f = np.load(r"D:\我的资源\下载位置\mnist.npz")
 # x, y = f['x_train'], f['y_train']
@@ -12,5 +12,22 @@ import numpy as np
 (x, y), (x_val, y_val) = datasets.mnist.load_data()
 x = 2 * tf.convert_to_tensor(x, dtype=tf.float32) / 255. - 1
 y = tf.convert_to_tensor(y, dtype=tf.int32)
-y = tf.one_hot(y, depth=10)
 print(x.shape, y.shape)
+
+layers.Dense(256, activation='relu')
+model = tf.keras.Sequential([
+    layers.Dense(256, activation='relu'),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(10)
+])
+
+with tf.GradientTape() as tape:
+    x = tf.reshape(x, (-1, 28 * 28))
+    out = model(x)
+    y_onehot = tf.one_hot(y, depth=10)
+
+    # 计算差的平方和
+    loss = tf.square(out - y_onehot)
+    loss = tf.reduce_sum(loss) / x.shape[0]
+    grads = tape.gradient(loss, model.trainable_variables)
+    optimizers.Optimizer.apply_gradient(loss, model.trainable_variables)
