4c8059630e65c06828dd51d31be2679915d13a75
bearbaba <2200522850@qq.com>
2020-07-13 19:36:45 +0800

    自闭了

--- fashionMnist/demo.py
+++ fashionMnist/demo.py
@@ -17,8 +17,8 @@ batch_size = 128
 db = tf.data.Dataset.from_tensor_slices((x, y))
 db = db.map(preprocess).shuffle(10000).batch(batch_size)
 
-db = tf.data.Dataset.from_tensor_slices((x_test, y_test))
-db = db.map(preprocess).batch(batch_size)
+db_test = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+db_test = db.map(preprocess).batch(batch_size)
 
 db_iter = iter(db)
 samples = next(db_iter)
@@ -34,3 +34,20 @@ model = Sequential([
 
 model.build(input_shape=[None, 28*28])
 model.summary()
+optimizer = optimizers.Adam(1e-3)
+for epoch in range(30):
+    for step, (x, y) in enumerate(db):
+        x = tf.reshape(x, [-1, 28*28])
+
+        with tf.GradientTape() as tape:
+            logits = model(x)
+            y_onehot = tf.one_hot(y, depth=10)
+
+            loss_mse = tf.reduce_mean(tf.losses.MSE(y_onehot, logits))
+            loss_ce = tf.reduce_mean(tf.losses.categorical_crossentropy(y_onehot, logits, from_logits=True))
+
+        grads = tape.gradient(loss_ce, model.trainable_variables)
+        optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+        if step % 100 ==0:
+            print(step,epoch,"loss:",float(loss_ce), float(loss_mse))
