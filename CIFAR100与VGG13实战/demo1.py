c315e7b9ee7f4544745ef1057b97ab06c9b67f7a
bearbaba <2200522850@qq.com>
2020-07-22 11:34:34 +0800

    现在另一处麻烦是它是怎么做到分类的

---
+++ CIFAR100与VGG13实战/demo1.py
@@ -0,0 +1,73 @@
+import tensorflow as tf
+from tensorflow.keras import layers, optimizers, datasets, Sequential
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+tf.random.set_seed(2345)
+
+conv_layers = [
+    # unit1
+    layers.Conv2D(64, kernel_size=[3, 3], padding='same', activation=tf.nn.relu),
+    layers.Conv2D(64, kernel_size=[3, 3], padding='same', activation=tf.nn.relu),
+    layers.MaxPool2D(pool_size=[2, 2], strides=2, padding='same'),
+
+    # unit2
+    layers.Conv2D(128, kernel_size=[3, 3], padding='same', activation=tf.nn.relu),
+    layers.Conv2D(128, kernel_size=[3, 3], padding='same', activation=tf.nn.relu),
+    layers.MaxPool2D(pool_size=[2, 2], strides=2, padding='same'),
+
+    # unit3
+    layers.Conv2D(256, kernel_size=[3, 3], padding='same', activation=tf.nn.relu),
+    layers.Conv2D(256, kernel_size=[3, 3], padding='same', activation=tf.nn.relu),
+    layers.MaxPool2D(pool_size=[2, 2], strides=2, padding='same'),
+
+    # unit4
+    layers.Conv2D(512, kernel_size=[3, 3], padding='same', activation=tf.nn.relu),
+    layers.Conv2D(512, kernel_size=[3, 3], padding='same', activation=tf.nn.relu),
+    layers.MaxPool2D(pool_size=[2, 2], strides=2, padding='same'),
+
+    # unit5
+    layers.Conv2D(512, kernel_size=[3, 3], padding='same', activation=tf.nn.relu),
+    layers.Conv2D(512, kernel_size=[3, 3], padding='same', activation=tf.nn.relu),
+    layers.MaxPool2D(pool_size=[2, 2], strides=2, padding='same'),
+]
+
+
+def preprocess(x, y):
+    x = tf.cast(x, dtype=tf.float32) / 255.
+    y = tf.cast(y, dtype=tf.int32)
+    return x, y
+
+
+(x, y), (x_test, y_test) = datasets.cifar100.load_data()
+y = tf.squeeze(y, axis=1)
+y_test = tf.squeeze(y_test, axis=1)
+
+print(x.shape, y.shape, x_test.shape, y_test.shape)
+
+train_db = tf.data.Dataset.from_tensor_slices((x, y))
+train_db = train_db.shuffle(1000).map(preprocess).batch(64)
+
+test_db = tf.data.Dataset.from_tensor_slices((x, y))
+test_db = test_db.shuffle(1000).map(preprocess).batch(64)
+
+
+def main():
+    conv_net = Sequential(conv_layers)
+    conv_net.build(input_shape=[None, 32, 32, 3])
+    x = tf.random.normal([4, 32, 32, 3])
+    out = conv_net(x)
+    print(out.shape)
+
+    fc_net = Sequential([
+        layers.Dense(256, activation=tf.nn.relu),
+        layers.Dense(128, activation=tf.nn.relu),
+        layers.Dense(100, activation=None),
+    ])
+
+    conv_net.build(input_shape=[None, 32, 32, 3])
+    fc_net.build(input_shape=[None, 512])
+
+
+if __name__ == '__main__':
+    main()
--- keras实战demo/demo1.py
+++ keras实战demo/demo1.py
@@ -8,7 +8,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
 def preprocess(x, y):
-    x = tf.cast(x, dtype=tf.float32) / 255
+    x = 2*tf.cast(x, dtype=tf.float32) / 255. - 1
     y = tf.cast(y, dtype=tf.int32)
     return x, y
 
@@ -46,10 +46,10 @@ class MyNetwork(keras.Model):
     def __init__(self):
         super(MyNetwork, self).__init__()
         self.fc1 = MyDense(32 * 32 * 3, 256)
-        self.fc2 = MyDense(256, 128)
-        self.fc3 = MyDense(128, 64)
-        self.fc4 = MyDense(64, 32)
-        self.fc5 = MyDense(32, 10)
+        self.fc2 = MyDense(256, 256)
+        self.fc3 = MyDense(256, 256)
+        self.fc4 = MyDense(256, 256)
+        self.fc5 = MyDense(256, 10)
 
     def call(self, inputs, training=None):
         x = tf.reshape(inputs, [-1, 32 * 32 * 3])
@@ -70,3 +70,16 @@ network.compile(optimizer=optimizers.Adam(lr=1e-3),
                 loss=tf.losses.CategoricalCrossentropy(from_logits=True),
                 metrics=['accuracy'])
 network.fit(train_db, epochs=5, validation_data=test_db, validation_freq=1)
+
+network.evaluate(test_db)
+network.save_weights('./weights.ckpt')
+del network
+
+network=MyNetwork()
+network.compile(optimizer=optimizers.Adam(lr=1e-3),
+                loss=tf.losses.CategoricalCrossentropy(from_logits=True),
+                metrics=['accuracy'])
+network.load_weights('./weights.ckpt')
+print("load the ckpt")
+network.evaluate(test_db)
+
